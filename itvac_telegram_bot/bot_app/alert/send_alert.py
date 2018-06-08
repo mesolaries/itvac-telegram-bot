98b9965c50a58472adf98bf6e09e2f55fb3bb0ff
Emil <mnf.emil@gmail.com>
2018-06-09 02:03:52 +0400

    Fixed alert delay & code improvements

--- itvac_telegram_bot/bot_app/alert/send_alert.py
+++ itvac_telegram_bot/bot_app/alert/send_alert.py
@@ -18,6 +18,26 @@ import json
 import os
 import time
 
+# Bot's alert messages
+
+first_message = "*Hey! This is a job alert!*"
+
+no_vacancies_message = "*There're no new vacancies for the last day.*"
+
+single_vacancy_warning_message = ("*There's a new vacancy for the last day!\n"
+                                    "I'm sending it to you now...*")
+
+plural_vacancy_warning_message = ("*There're {} new vacancies for the last day!\n"
+                                    "I'm sending them to you now...*")
+
+vacancy_message = ("*Vacancy #{count}* \n\n"
+                    "*Title:* {title} \n"
+                    "*Company:* {company} \n"
+                    "*Location:* {location} \n"
+                    "*Salary:* {salary} \n"
+                    "*Overview:* \n{overview} \n\n"
+                    "{url} ")
+
 
 def send_alert():
     db = UserModel(dbname='../../db.sqlite3')
@@ -32,63 +52,54 @@ def send_alert():
             json.dump(new_data, outfile, indent=4, ensure_ascii=False)
     # Sending vacancies
     for user in user_list:
-        send_message(chat_id=user,
-                     text="*Hey! This is a job alert!*"
-                     )
+        send_message(chat_id=user, text=first_message)
         count = len(new_data)
         if count == 0:
             send_message(chat_id=user,
-                         text="*There're no new vacancies for the last day.*"
+                         text=no_vacancies_message
                          )
         else:
             if count == 1:
                 send_message(chat_id=user,
-                             text="*There's a new vacancy for the last day!\n"
-                                  "I'm sending it to you now...*".format(count)
+                             text=single_vacancy_warning_message.format(count)
                              )
             else:
                 send_message(chat_id=user,
-                             text="*There're {} new vacancies for the last day!\n"
-                                  "I'm sending them to you now...*".format(count)
+                             text=plural_vacancy_warning_message.format(count)
                              )
-            time.sleep(3)  # Wait 3 secs, then send vacancies
             for vacancy in new_data:
                 send_message(chat_id=user,
-                             text=
-                                     "*Vacancy #{count}* \n\n"
-                                     "*Title:* {title} \n"
-                                     "*Company:* {company} \n"
-                                     "*Location:* {location} \n"
-                                     "*Salary:* {salary} \n"
-                                     "*Overview:* \n{overview} \n\n"
-                                     "{url} ".format(
-                                         count=count,
-                                         title=vacancy['title'],
-                                         company=vacancy['company'],
-                                         location=vacancy['location'],
-                                         salary=vacancy['salary'],
-                                         overview=vacancy['overview'],
-                                         url=vacancy['url']
-                                     )
+                             text=vacancy_message.format(
+                                                    count=count,
+                                                    title=vacancy['title'],
+                                                    company=vacancy['company'],
+                                                    location=vacancy['location'],
+                                                    salary=vacancy['salary'],
+                                                    overview=vacancy['overview'],
+                                                    url=vacancy['url']
+                                                    )
                              )
                 count -= 1
-                time.sleep(1)  # Wait a sec before the next vacancy
+                time.sleep(.1)  # Wait 0.1 sec before the next vacancy
 
 
 # Method for comparing new data to last sent
 def compare_data(new_data):
-    with open('last_sent_vacancies.json', 'r+') as f:
-        if not os.stat("last_sent_vacancies.json").st_size:
-            return new_data
-        else:
-            old_data = json.load(f)  # Get last sent vacancies
-            for old_vacancy in old_data:
-                # TODO: Implement binary search algorithm here
-                if old_vacancy in new_data:
-                    # Delete vacancy from data if the vacancy exists in last sent vacancies
-                    new_data.pop(new_data.index(old_vacancy))
-                else:
-                    continue
+    try:
+        with open('last_sent_vacancies.json', 'r+') as f:
+            if not os.stat("last_sent_vacancies.json").st_size:
+                return new_data
+            else:
+                old_data = json.load(f)  # Get last sent vacancies
+                for old_vacancy in old_data:
+                    # TODO: Implement binary search algorithm here
+                    if old_vacancy in new_data:
+                        # Delete vacancy from data if the vacancy exists in last sent vacancies
+                        new_data.pop(new_data.index(old_vacancy))
+                    else:
+                        continue
+    except FileNotFoundError:
+        return new_data
     return new_data
 
 
