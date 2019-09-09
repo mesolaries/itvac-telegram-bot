ee2d22e2cf2429f69d66957a23980281f532091a
unknown <pr.tester@PR_EMIL_NB.dc.loc>
2019-09-09 15:41:42 +0500

    Bug fixes

--- itvac_telegram_bot/bot_app/alert/vacancies.py
+++ itvac_telegram_bot/bot_app/alert/vacancies.py
@@ -21,7 +21,8 @@ def get_new_vacancies():
     boss_az = __BossAz()
     azerjobs_com = __AzerjobsCom()
     # Sum of all vacancies available
-    vacancies = rabota_az.vacancies() + day_az.vacancies() + boss_az.vacancies() + azerjobs_com.vacancies()
+    vacancies = rabota_az.vacancies() + day_az.vacancies() + \
+        boss_az.vacancies() + azerjobs_com.vacancies()
     return vacancies
 
 
@@ -53,7 +54,8 @@ class __RabotaAz:
             soup = BeautifulSoup(page.content, 'lxml')
             data = {'results': []}
             try:
-                vacancies = soup.find('ul', {'class': 'visitor-vacancy-list'}).find_all('li')
+                vacancies = soup.find(
+                    'ul', {'class': 'visitor-vacancy-list'}).find_all('li')
             except AttributeError:
                 return data['results']
             for vacancy in vacancies:
@@ -62,10 +64,14 @@ class __RabotaAz:
                 if salary == "Əmək haqqı müsahibə əsasında":
                     salary = "Not specified"
                 location = vacancy.find('b', {'class': 'address-'}).get_text()
-                company = vacancy.find('span', {'class': 'company-'}).b.get_text()
-                overview = vacancy.find_all('p')[-1].get_text()  # Overview is in the last <p>
-                overview = overview.replace("\t", '')[1:]  # [1:0] removes new line at the beginning
-                url = self.base_url + vacancy.find('a', {'class': 'title-'}).get('href')
+                company = vacancy.find(
+                    'span', {'class': 'company-'}).b.get_text()
+                # Overview is in the last <p>
+                overview = vacancy.find_all('p')[-1].get_text()
+                # [1:0] removes new line at the beginning
+                overview = overview.replace("\t", '')[1:]
+                url = self.base_url + \
+                    vacancy.find('a', {'class': 'title-'}).get('href')
                 data['results'].append(
                     {
                         "title": title, "salary": salary, "location": location,
@@ -103,21 +109,29 @@ class __DayAz:
             soup = BeautifulSoup(page.content, 'lxml')
             data = {'results': []}
             try:
-                vacancies = soup.find('div', {'class': 'vacancy-serp'}).find_all('div', {'class': 'vacancy-serp-item'})
+                vacancies = soup.find(
+                    'div', {'class': 'vacancy-serp'}).find_all('div', {'class': 'vacancy-serp-item'})
             except AttributeError:
                 return data['results']
             for vacancy in vacancies:
-                title = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).get_text()
+                title = vacancy.find(
+                    'a', {'data-qa': 'vacancy-serp__vacancy-title'}).get_text()
                 try:
-                    salary = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy-compensation'}).get_text()
+                    salary = vacancy.find(
+                        'div', {'data-qa': 'vacancy-serp__vacancy-compensation'}).get_text()
                 except:
                     salary = "Not specified"
-                location = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-address'}).get_text()
-                company = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'}).get_text()
-                requirements = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).get_text()
-                responsibility = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).get_text()
+                location = vacancy.find(
+                    'span', {'data-qa': 'vacancy-serp__vacancy-address'}).get_text()
+                company = vacancy.find(
+                    'a', {'data-qa': 'vacancy-serp__vacancy-employer'}).get_text()
+                requirements = vacancy.find(
+                    'div', {'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).get_text()
+                responsibility = vacancy.find(
+                    'div', {'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).get_text()
                 overview = requirements + '\n' + responsibility
-                url = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).get('href')
+                url = vacancy.find(
+                    'a', {'data-qa': 'vacancy-serp__vacancy-title'}).get('href')
                 data['results'].append(
                     {
                         "title": title, "salary": salary, "location": location,
@@ -141,10 +155,12 @@ class __BossAz:
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36"
     # No daily/weekly filter on website
     url = "https://boss.az/vacancies?search%5Bcategory_id%5D=38"
-    base_url = "https://en.boss.az"  # Using english version of site to get date in right format
+    # Using english version of site to get date in right format
+    base_url = "https://en.boss.az"
     base_url_az = "https://boss.az"
 
-    def vacancies(self, url=url, offset=0):  # offset - vacancy post date period (e.g. 7 = for the past week, 0 - today)
+    # offset - vacancy post date period (e.g. 7 = for the past week, 0 - today)
+    def vacancies(self, url=url, offset=0):
         page = requests.get(url, headers={'User-agent': self.user_agent})
         if page.status_code == 200:
             soup = BeautifulSoup(page.content, 'lxml')
@@ -155,10 +171,13 @@ class __BossAz:
                 all_vacancies = soup.find_all('div', {'class': 'results-i'})
                 count = 0
                 for vac in all_vacancies:
-                    link = vac.find('a', {'class': 'results-i-link'}).get('href')
-                    inner_vac = requests.get(self.base_url + link, headers={'User-agent': self.user_agent})
+                    link = vac.find(
+                        'a', {'class': 'results-i-link'}).get('href')
+                    inner_vac = requests.get(
+                        self.base_url + link, headers={'User-agent': self.user_agent})
                     inner_soup = BeautifulSoup(inner_vac.content, 'lxml')
-                    vac_date = inner_soup.find('div', {'class': 'bumped_on params-i-val'}).get_text()
+                    vac_date = inner_soup.find(
+                        'div', {'class': 'bumped_on params-i-val'}).get_text()
                     vac_date = datetime.strptime(vac_date, "%B %d, %Y").date()
                     #  Count vacancy if it's in a given time period (offset)
                     if today <= vac_date + timedelta(days=offset):
@@ -170,13 +189,19 @@ class __BossAz:
                 return data['results']
             for vacancy in vacancies:
                 title = vacancy.h3.get_text()
-                salary = vacancy.find('div', {'class': 'results-i-salary'}).get_text()
+                salary = vacancy.find(
+                    'div', {'class': 'results-i-salary'}).get_text()
                 if salary == "—" or salary == "-":
                     salary = "Not specified"
-                location = str(vacancy.find('div', {'class': 'results-i-secondary'}).contents[0])
-                company = vacancy.find('a', {'class': 'results-i-company'}).get_text()
-                overview = vacancy.find('div', {'class': 'results-i-summary'}).p.get_text()
-                url = self.base_url_az + vacancy.find('a', {'class': 'results-i-link'}).get('href')  # Send user AZ version of site
+                location = str(vacancy.find(
+                    'div', {'class': 'results-i-secondary'}).contents[0])
+                company = vacancy.find(
+                    'a', {'class': 'results-i-company'}).get_text()
+                overview = vacancy.find(
+                    'div', {'class': 'results-i-summary'}).p.get_text()
+                # Send user AZ version of site
+                url = self.base_url_az + \
+                    vacancy.find('a', {'class': 'results-i-link'}).get('href')
                 data['results'].append(
                     {
                         "title": title, "salary": salary, "location": location,
@@ -200,8 +225,9 @@ class __AzerjobsCom:
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36"
     url_daily = "https://www.azerjobs.com/search-posting?type=1&category=13&published=0"
     url_weekly = "https://www.azerjobs.com/search-posting?type=1&category=13&published=7"
-    base_url = "https://www.azerjobs.com"  
-    url_set_lang = "https://www.azerjobs.com/set-language/az" # Get website in azerbaijani
+    base_url = "https://www.azerjobs.com"
+    # Get website in azerbaijani
+    url_set_lang = "https://www.azerjobs.com/set-language/az"
 
     def vacancies(self, url=url_daily):
         session = requests.Session()
@@ -213,23 +239,29 @@ class __AzerjobsCom:
             data = {'results': []}
             vacancy_links = []
             try:
-            	try:
-                	vacancies = soup.find_all('table', {'class': 'vacanc'})[1].find('tbody').find_all('tr')
+                try:
+                    vacancies = soup.find_all('table', {'class': 'vacanc'})[
+                        1].find('tbody').find_all('tr')
                 except IndexError:
-                	vacancies = soup.find_all('table', {'class': 'vacanc'})[0].find('tbody').find_all('tr')
+                    vacancies = soup.find_all('table', {'class': 'vacanc'})[
+                        0].find('tbody').find_all('tr')
                 finally:
                     for vacancy in vacancies:
-                        link = vacancy.find('a', {'class': 'title'}).get('href')
+                        link = vacancy.find(
+                            'a', {'class': 'title'}).get('href')
                         vacancy_links.append(link)
             except AttributeError:
                 return data['results']
             for link in vacancy_links:
-                vac_page = session.get(self.base_url + link, headers={'User-agent': self.user_agent})
+                vac_page = session.get(
+                    self.base_url + link, headers={'User-agent': self.user_agent})
                 if vac_page.status_code == 200:
                     soup = BeautifulSoup(vac_page.content, 'lxml')
                     vacancy = soup.find('div', {'class', 'anncmt-block'})
-                    title = vacancy.find('div', {'class': 'anncmt-title'}).h1.get_text()
-                    details = vacancy.find('div', {'class': 'posting-details'}).find_all('table', {'class': 'firm-card'})
+                    title = vacancy.find(
+                        'div', {'class': 'anncmt-title'}).h1.get_text()
+                    details = vacancy.find(
+                        'div', {'class': 'posting-details'}).find_all('table', {'class': 'firm-card'})
                     for detail_group in details:
                         detail_group_items = detail_group.find_all('tr')
                         for detail in detail_group_items:
@@ -240,11 +272,13 @@ class __AzerjobsCom:
                             if option_text == "Əmək haqqı:":
                                 salary = option.next_sibling.strip()
                             elif option_text == "Yerləşmə:":
-                                location = option.find_next_sibling().get_text().strip() 
+                                location = option.find_next_sibling().get_text().strip()
                     if 'salary' not in locals():
                         salary = 'Not specified'
-                    company = vacancy.find('div', {'class': 'anncmt-customer'}).h2.a.get_text().strip()
-                    overview = vacancy.find('div', {'class': 'firm-descr'}).find_all('ul')[0].get_text().strip()
+                    company = vacancy.find(
+                        'div', {'class': 'anncmt-customer'}).h2.a.get_text().strip()
+                    overview = vacancy.find(
+                        'div', {'class': 'firm-descr'}).find_all('ul')[0].get_text().strip()
                     overview = overview[:435] + "..."
                     url = self.base_url + link
                     data['results'].append(
